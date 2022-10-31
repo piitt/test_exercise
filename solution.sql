662ffc02adb9a4e23d50e97e1a242499506adca5
Alexandr Plakhotnikov <mxplaha@gmail.com>
2022-10-31 14:10:43 +0300

    full solution

--- solution.sql
+++ solution.sql
@@ -1,3 +1,19 @@
+CREATE VIEW phone_a AS
+SELECT
+    s.id,
+    s.phone,
+    t.tariff_name
+FROM
+    subscriber AS s
+    JOIN tariff_plan AS t ON s.trpl_id = t.id;
+CREATE VIEW phone_b AS
+SELECT
+    s.id,
+    s.phone,
+    t.tariff_name
+FROM
+    subscriber AS s
+    JOIN tariff_plan AS t ON s.trpl_id = t.id;
 SELECT
     c.date AS date,
     pa.phone AS 'phone a',
@@ -8,5 +24,5 @@ SELECT
     c.price AS price
 FROM
     call AS c
-        JOIN phone_a AS pa ON pa.id = c.id_a
-        JOIN phone_b AS pb ON pb.id = c.id_b;
+    JOIN phone_a AS pa ON pa.id = c.id_a
+    JOIN phone_b AS pb ON pb.id = c.id_b;
